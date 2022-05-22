dc4b3e77cd6260d49976f994e5bf50fe0b94f562
yoga163 <yoga.nugroho166@gmail.com>
2022-05-22 18:00:21 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <main class="h-screen">
+    <div class="content-center">
+      <h1 class="text-6xl font-bold">Country</h1>
+      
+    </div>
+  </main>
+</template>
+
+<script setup>
+import TheWelcome from '@/components/HelloWorld.vue'
+</script>
